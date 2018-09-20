96a75eba985c7ba40fad63603407cfea7b9d853f
italo <italotabatinga@gmail.com>
2018-09-20 04:23:45 -0300

    Added server/client features

--- Client/client.py
+++ Client/client.py
@@ -1,5 +1,7 @@
 """Class that handle client connections"""
 import socket
+import json
+from random import randint
 from threading import Thread, Event
 UDP_PORT = 13702
 TCP_PORT = 13703
@@ -11,29 +13,38 @@ class Client:
 
     self.udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     self.udp.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    udp_src = (ip_addr, 10001)
+    udp_src = ('', randint(10000, 20000))
     self.udp.bind(udp_src)
 
-    tcp_src = (ip_addr, 10002)
+    tcp_src = (ip_addr, randint(10000, 20000))
     self.tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.tcp.bind(tcp_src)
 
     self.available_servers = []
 
     self.timeout_handle = Event()
 
   def wait_servers_response(self):
+    """Function that handles servers response to broadcast"""
     while True:
       data, _ = self.udp.recvfrom(1024)
-      print("received", data)
-
+      self.available_servers.append(json.loads(data.decode()))
       if self.timeout_handle.is_set():
         break
 
+  def tcp_connect(self, addr):
+    self.tcp.connect(addr)
+    msg = input()
+    while msg != 'exit':
+      self.tcp.send(msg.encode())
+      msg = input()
+    self.tcp.close()
+
   def search_servers(self):
     """Send udp broadcast to search for servers"""
+    self.available_servers = []
     self.udp.sendto(b"MANDANDO BROADCAST", ('<broadcast>', UDP_PORT))
     wait_response = Thread(target=self.wait_servers_response)
     wait_response.start()
     wait_response.join(timeout=1)
     self.timeout_handle.set()
+    print("received", self.available_servers)
--- Server/server.py
+++ Server/server.py
@@ -1,5 +1,7 @@
 """Class that handle server connections"""
 import socket
+import json
+from threading import Thread, Event
 
 UDP_PORT = 13702
 TCP_PORT = 13703
@@ -11,16 +13,47 @@ class Server:
 
     self.udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     self.udp.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    udp_src = (ip_addr, UDP_PORT)
+    udp_src = ('', UDP_PORT)
     self.udp.bind(udp_src)
 
     tcp_src = (ip_addr, TCP_PORT)
     self.tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.tcp.bind(tcp_src)
+    self.tcp.listen()
 
-  def wait_broad(self):
-    """Wait broadcast"""
+    self.bc_response = self.gen_bc_response()
+    self.num_conn = 0
+
+  def gen_bc_response(self):
+    """Return model to broadcast response"""
+    return {
+        'tcp_addr': self.tcp.getsockname()
+    }
+
+  def respond_bc(self):
     while True:
       data, addr = self.udp.recvfrom(1024)
-      print("received", data)
-      self.udp.sendto(b"RESPONDENDO BROADCAST", addr)
+      print("received", data, "from", addr)
+      self.udp.sendto(json.dumps(self.bc_response).encode(), addr)
+      if self.num_conn == 2:
+        break
+
+  def tcp_conn(self):
+    conn, client = self.tcp.accept()
+    print(conn, client)
+    self.num_conn += 1
+    while True:
+      msg = conn.recv(1024).decode()
+      if not msg:
+        break
+      print(client, msg)
+    print('CABOU')
+
+  def wait_conn(self):
+    """Wait connections from two players"""
+    udp_conn = Thread(target=self.respond_bc)
+    udp_conn.start()
+    tcp_conn1 = Thread(target=self.tcp_conn)
+    tcp_conn1.start()
+    tcp_conn2 = Thread(target=self.tcp_conn)
+    tcp_conn2.start()
