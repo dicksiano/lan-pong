56e998c78f2ce97c2f316696f461ed2e5d8cc34f
italo <italotabatinga@gmail.com>
2018-09-20 19:38:25 -0300

    Added exit features and refactoring

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 """Main module to run Pong-Lan game."""
 import sys
-from os import path, getcwd
+from os import path
 from math import floor
 import pygame
-from pong_menu import PongMenu
+import socket
+from Client.pong_menu import PongMenu
 
 # States
 INGAME = 0
@@ -29,8 +30,8 @@ if __name__ == "__main__":
   screen = pygame.display.set_mode((WIDTH, HEIGHT))
   pygame.display.set_caption('Pong-Lan')
   state = MENU
-  
-  FONT = pygame.font.Font(path.join(getcwd(), "src", "bit5x3.ttf"), 12)
+
+  FONT = pygame.font.Font(path.join(path.dirname(__file__), "Client", "src", "bit5x3.ttf"), 12)
   menu = PongMenu()
   active_scene = menu
 
@@ -40,12 +41,27 @@ if __name__ == "__main__":
     #   pass
     # elif state == INGAME:
     #   pass
+    pressed_keys = pygame.key.get_pressed()
+    filtered_events = []
     for event in pygame.event.get():
+      quit_attempt = False
       if event.type == pygame.QUIT:
+        quit_attempt = True
+      elif event.type == pygame.KEYDOWN:
+        alt_pressed = pressed_keys[pygame.K_LALT] or pressed_keys[pygame.K_RALT]
+        if event.key == pygame.K_ESCAPE:
+          quit_attempt = True
+        elif event.key == pygame.K_F4 and alt_pressed:
+          quit_attempt = True
+
+      if quit_attempt:
         pygame.quit()
         sys.exit()
+      else:
+        filtered_events.append(event)
+
     screen.fill(BLACK)
-    active_scene.update()
+    active_scene.update(filtered_events, pressed_keys)
     active_scene.render(screen)
     render_fps(FONT, screen, floor(clock.get_fps()))
     pygame.display.update()
